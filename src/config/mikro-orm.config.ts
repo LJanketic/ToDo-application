b91483494bc37f3eb8b5a4f027ec63945438c27b
Luka Janketić <luka.janketic@gmail.com>
2024-04-19 16:06:43 +0200

    create a simple config for the database

---
+++ src/config/mikro-orm.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@mikro-orm/mongodb';
+import { MongoHighlighter } from '@mikro-orm/mongo-highlighter';
+import { BaseEntity, ToDoEntity } from '../shared/entities';
+
+export default defineConfig({
+  entities: [BaseEntity, ToDoEntity],
+  dbName: 'mikro-orm-express-ts',
+  highlighter: new MongoHighlighter(),
+  debug: true,
+});
