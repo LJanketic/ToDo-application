a09940e32ffe2111d2d41af5b6703659013cfcca
Luka Janketić <luka.janketic@gmail.com>
2024-04-19 16:19:47 +0200

    silly goose mistakes

--- src/shared/entities/base-entity.ts
+++ src/shared/entities/base-entity.ts
@@ -3,7 +3,7 @@ import { v4 as uuid } from 'uuid';
 
 export abstract class BaseEntity {
   @PrimaryKey({ type: 'uuid' })
-  id: string = uuid();
+  _id: string = uuid();
 
   @Property()
   createdAt: Date = new Date();
